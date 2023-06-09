4fecb02f9b8ce0c7e0c130c9b9e3929c6214a3ad
VishalShenoy2002 <vishal.bshenoy@gmail.com>
2023-06-09 08:59:12 +0530

    added function of read_and_insert batch

--- uploader.py
+++ uploader.py
@@ -1,10 +1,36 @@
 import mysql.connector as mysql
 import csv
+import json
 import os
 
+import util_functions
 
-# Upload Table Structure
-# Timestamp
-# File Name
-# Size
+
+with open("config.json","r") as f:
+    data=json.load(f)
+    f.close()
+
+
+connection=mysql.connect(host=data['db']['host'],user=data['db']['user'],passwd=data['db']['password'],database=data['db']['database'])
+cursor=connection.cursor()
+
+def read_and_insert_batch(csvfile:str):
+    with open(csvfile,"r") as f:
+        reader=csv.reader(f)
+
+        records=[row for row in reader]
+        records=[util_functions.insert_into_student(row) for row in records]
+
+        for index,query in enumerate(records,start=1):
+            try:
+                cursor.execute(query)
+                print(f"{index} records inserted.",end='\r')
+            except:
+                continue
+
+        connection.commit()
+
+
+
+
 
