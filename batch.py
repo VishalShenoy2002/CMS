8a37f5d1a963f22a21c5d24207154eaceea44d46
VishalShenoy2002 <vishal.bshenoy@gmail.com>
2023-06-26 00:29:35 +0530

    removed print statements

--- batch.py
+++ batch.py
@@ -18,21 +18,18 @@ class Batch:
 
     def set_end_year(self):
         query=f'SELECT num_of_sems FROM courses WHERE course_name="{self.department}";'
-        print(query)
         self._cursor.execute(query)
 
         self.no_of_sems=self._cursor.fetchone()[0]
         self.end_year=int(self.start_year) + (int(self.no_of_sems)//2)
-        # print(self.end_year)
+
 
     def generate_batch_code(self):
         short_start_year=self.start_year % 2000
         short_end_year=self.end_year % 2000
-        
-        print(short_end_year,short_start_year)
+
         self.year_code=f"{short_start_year}{short_end_year}"
         code=f"NEP-{self.department}-{self.year_code}"
-        print(code)
         return code
     
     def get_year_code(self):
@@ -42,7 +39,6 @@ class Batch:
     
     def create_batch(self):
         batch_code=self.generate_batch_code()
-        print(batch_code)
         year_code=self.get_year_code()
         record=(batch_code,self.start_year,self.end_year,self.department,year_code)
         db_functions.insert_batch(record)
