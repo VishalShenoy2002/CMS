6fdbe94a3a0b086c080bbfc10dff7296ccc2f934
VishalShenoy2002 <vishal.bshenoy@gmail.com>
2023-06-30 07:09:58 +0530

    added the courses module which has the courses class which handles the courses

---
+++ course.py
@@ -0,0 +1,39 @@
+import mysql.connector as mysql
+import json
+import db_functions
+
+
+class Course:
+    def __init__(self):
+        self.course_name: str = None
+        with open("config.json", "r") as f:
+            data = json.load(f)
+            data = data['db']
+            f.close()
+
+        self._connection = mysql.connect(host=data['host'], user=data['user'], passwd=data['password'], database=data['database'])
+        self._cursor = self._connection.cursor()
+
+    def course_count(self):
+        query = "SELECT count(*) FROM courses;"
+        self._cursor.execute(query)
+
+        count = self._cursor.fetchone()[0]
+        del query
+        return count
+
+    def courses_available(self):
+        query=f'SELECT course_name FROM courses;'
+        self._cursor.execute(query)
+
+        courses=[x[0] for x in self._cursor.fetchall()]
+        del query
+        return courses
+
+    def exists(self):
+        return self.course_count() != 0
+
+        
+
+
+Course().courses_available()
